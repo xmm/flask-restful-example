ffa8985b311667d4930d95aa2e76de66247d5a91
Marat Khayrullin <xmm.dev@gmail.com>
2014-03-16 04:52:04 +0400

    configuration looks to envvar FLASK_CONFIG

--- api/__init__.py
+++ api/__init__.py
@@ -3,15 +3,25 @@
 
 from flask import Flask
 import flask_restful
+import os
 
 #from api.database import db
 from api.v1 import api_v1, api_v1_bp, API_VERSION_V1
 from api.v2 import api_v2, api_v2_bp, API_VERSION_V2
 
 
-def create_app(environment="Development"):
+def create_app(environment=None):
     app = Flask(__name__)
-    app.config.from_object('api.config.{}'.format(environment))
+
+    if not environment:
+        environment = os.environ.get('FLASK_CONFIG')
+    if not environment:
+        environment = 'development'
+    app.config.from_object('api.config.{}'.format(environment.capitalize()))
+    app.config.from_pyfile(
+        'config_{}.py'.format(environment.lower()),
+        silent=True
+        )
 
     app.register_blueprint(
         api_v1_bp,
--- api/config.py
+++ api/config.py
@@ -7,24 +7,25 @@ from os import path
 class Config(object):
     DEBUG = False
     PORT = 5000
-    HOST = "0.0.0.0"
+    HOST = '0.0.0.0'
     URL_PREFIX = '/api'
     PROJECT_ROOT = path.abspath(path.dirname(__file__))
     TEMPLATE_FOLDER = path.join(PROJECT_ROOT, 'templates')
+    MYSQL_DATABASE_HOST = '127.0.0.1'
+    MYSQL_DATABASE_DB = 'default_db'
+    MYSQL_DATABASE_USER = 'default_user'
+    MYSQL_DATABASE_PASSWORD = 'password'
 
 
 class Development(Config):
     DEBUG = True
+    SECRET_KEY = 'development'
 
 
 class Production(Config):
-    SQLALCHEMY_ECHO = False
-    MYSQL_DATABASE_HOST = '10.0.0.1'
-    MYSQL_DATABASE_DB = 'db'
-    MYSQL_DATABASE_USER = 'user'
-    MYSQL_DATABASE_PASSWORD = 'password'
+    pass
 
 
 class Testing(Config):
-    SECRET_KEY = "2147d2df-759b-40ac-8013-f6154110a7d0"
     TESTING = True
+    SECRET_KEY = 'testing'
